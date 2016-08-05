6781b3635a2894b95ceb84a6634dc78a3b553266
Rajdeep Surolia <rajdeep7@icloud.com>
2016-08-06 01:09:59 +0530

    Create Inorder.cpp

---
+++ Tree/Inorder.cpp
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> a;
+    
+    vector<int> inorderTraversal(TreeNode* root) {
+        if(root!=NULL) {
+            helper(root);
+        }
+        return a;
+    }
+   
+    void helper(TreeNode* p) {
+        if(p->left)
+            helper(p->left);
+            
+        a.push_back(p->val);
+            
+        if(p->right)
+            helper(p->right);
+    }
+};
