8a8e1152db9fd67c34ff499565eab99fa0c008fb
Rajdeep Surolia <rajdeep7@icloud.com>
2016-08-06 00:41:00 +0530

    Create CoinChange.cpp

---
+++ Dynamic-Programming/CoinChange.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int coinChange(vector<int>& coins, int amount) {
+        const int INF = 0x7fffffff;
+		vector<int> dp(amount + 1, INF);
+		dp[0] = 0;
+		for (int i = 0; i <= amount; i++) {
+			for (int j = 0; j<coins.size(); j++) {
+				if (i + coins[j] <= amount && dp[i]!= INF && dp[i + coins[j]] > dp[i] + 1)
+					dp[i + coins[j]] =dp[i] + 1;
+			}
+		}
+		return dp[amount] == INF ? -1 : dp[amount];
+    }
+};
