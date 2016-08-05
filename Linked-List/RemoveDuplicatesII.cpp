688f955d0d61e7a5482ae9c46a24b9ef115a4972
Rajdeep Surolia <rajdeep7@icloud.com>
2016-08-06 01:10:41 +0530

    Create RemoveDuplicatesII.cpp

---
+++ Linked-List/RemoveDuplicatesII.cpp
@@ -0,0 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* deleteDuplicates(ListNode* head) {
+        if ((head==NULL) || (head->next == NULL) ){return head;}
+        ListNode* pre = new ListNode(0);
+        pre->next = head;
+        head =pre;
+        ListNode* p = head;
+                    
+        while(p->next!=NULL){
+            ListNode *p2 = p->next;
+            while ((p2->next!=NULL)&&(p2->val==p2->next->val)){
+                p2=p2->next;
+            }
+            if (p2!=p->next){
+                p->next=p2->next;
+            }else{
+                p=p->next;
+            }
+        }  
+        return head->next;
+    }
+};
